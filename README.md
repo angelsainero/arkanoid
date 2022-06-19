4fbaf338dad5e624536ccc2c6651db3d89ef410c
Angel Sainero <angelsainero@gmail.com>
2022-06-19 14:02:51 +0200

    actualizamos archivo readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# arkanoid
-Replica del juego clásico de los ladrillos
+# Arkanoid
+
+Réplica del juego clásico de los ladrillos hecho con pygame
+
+## Para jugar
+
+1. Clonar el repositorio
+2. Crear un entorno virtual dentro de la raíz del repositorio
+3. Instalar las dependencias
+4. Arrancar el juego
+
+```
+# con ssh
+git clone ssh://.....
+# con https
+git clone https://.....
+cd arkanoid
+python -m venv env
+# windows
+.\env\Scripts\activate
+# linux / MacOs
+source ./env/bin/activate
+pip install -r requirements.txt
+python main.py
+```
+
+## Para colaborar en el proyecto
+
+1. Clonar el repositorio
+2. Crear un entorno virtual dentro de la raíz del repositorio
+3. Instalar las dependencias
+4. Abre el código en tu IDE favorito
+
+```
+# con ssh
+git clone ssh://.....
+# con https
+git clone https://.....
+cd arkanoid
+python -m venv env
+# windows
+.\env\Scripts\activate
+# linux / MacOs
+source ./env/bin/activate
+pip install -r requirements-dev.txt
+# En VS Code
+code .
+```
--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,20 +1,20 @@
-import pygame
+import pygame as pg #ponemos este alias para no escribir pygame todo el rato
 from arkanoid import ALTO, ANCHO
 
 class Arkanoid:
     def __init__(self) -> None:
         print("Arranca el juego!!")
-        pygame.init() #inicializar 
-        self.display=pygame.display.set_mode((ANCHO, ALTO)) #definir pantalla 
+        pg.init() #inicializar 
+        self.display=pg.display.set_mode((ANCHO, ALTO)) #definir pantalla 
     
     def jugar(self): #bucle principal
         salir = False 
         while not salir: #mientras no tengamos que salir nos quedamos en el bucle
-            for event in pygame.event.get(): #detectamos los eventos 
-                if event.type == pygame.QUIT: #si el tipo de evento es el del QUIT saliamos del bucle
+            for event in pg.event.get(): #detectamos los eventos 
+                if event.type == pg.QUIT: #si el tipo de evento es el del QUIT saliamos del bucle
                     salir=True
 
             self.display.fill((99,99,99)) #pintamos la pantalla de un color
-            pygame.display.flip() #para ver lo que pintamos 
+            pg.display.flip() #para ver lo que pintamos 
 
 
